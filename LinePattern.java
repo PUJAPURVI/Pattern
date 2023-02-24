ea6d55b5f5336a3715a60c1fb635c9a623977609
PUJAPURVI <nerkarpuja@gmail.com>
2023-02-24 23:39:20 +0530

    Added: Pattern

---
+++ LinePattern.java
@@ -0,0 +1,14 @@
+package com.pattern;
+
+public class LinePattern {
+    public static void main(String[] args) {
+        int n = 4;
+        n = 5;
+        for (int i = 0; i <= 4; i++) {
+            for (int j = i + 1; j <= 4; j++)
+                System.out.print("");
+
+
+        }
+    }
+}
